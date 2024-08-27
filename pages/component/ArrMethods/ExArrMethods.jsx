940d7d7a05cff060d62e81e130ba624ab3286d95
Paula Cano <paulacanoroman@gmail.com>
2024-08-27 18:02:29 +0200

    fat: js methods with for

--- pages/component/ArrMethods/ExArrMethods.jsx
+++ pages/component/ArrMethods/ExArrMethods.jsx
@@ -10,12 +10,29 @@ const ExArrMethods = () => {
     setUserSentence(newSent)
   }
 
+  const changeTextMethod1 = ()=>{
+
+    let words = userSentence.split(' ');
+    let reversedWords = [];
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i];
+      let reversedWord = '';
+      for (let j = word.length - 1; j >= 0; j--) {
+        reversedWord += word[j];
+      }
+      reversedWords.push(reversedWord);
+    }
+    let reversedSentence = reversedWords.join(' ');
+    setUserSentence(reversedSentence); 
+  }
+
   return (
     <div>
       {userSentence}
       <br></br>
       <input onChange={(e)=>{setUserSentence(e.target.value)}}></input>
       <button onClick={()=>{changeTextMyMethod()}}>My Method</button>    
+      <button onClick={()=>{changeTextMethod1()}}>Method 1</button>    
     </div>
   )
 }
